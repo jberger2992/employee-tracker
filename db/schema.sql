06323088cafceb067214c20e55ac3c75ff8b7f56
jberger2992 <jberger2992@gmail.com>
2023-04-30 23:37:11 -0400

    schema and seeds done

--- db/schema.sql
+++ db/schema.sql
@@ -2,25 +2,25 @@ DROP DATABASE IF EXISTS employees_db;
 CREATE DATABASE employees_db;
 USE employees_db;
 
+CREATE TABLE departments(
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL UNIQUE,
+    name VARCHAR(30)
+);
+
+CREATE TABLE roles(
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL UNIQUE,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL,
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES departments(id)
+);
+
 CREATE TABLE employees(
     id INT PRIMARY KEY AUTO_INCREMENT NOT NULL UNIQUE,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT,
     manager_id INT,
-    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (role_id) REFERENCES roles(id),
     FOREIGN KEY (manager_id) REFERENCES employees(id)
-);
-
-CREATE TABLE role(
-    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL UNIQUE,
-    title VARCHAR(30) NOT NULL,
-    salary DECIMAL,
-    department_id INT,
-    FOREIGN KEY (department_id) REFERENCES department(id)
-);
-
-CREATE TABLE department(
-    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL UNIQUE,
-    name VARCHAR(30)
 );
---
+++ db/seeds.sql
@@ -0,0 +1,30 @@
+USE employees_db;
+
+INSERT INTO departments (name)
+VALUES  ("Command"),
+        ("Opperations"),
+        ("Sciences");
+
+INSERT INTO roles (title, salary, department_id)
+VALUES  ("Captain", 100000, 1),
+        ("Pilot", 70000, 1),
+        ("Chief Engineer", 80000, 2),
+        ("Opperations Officer", 70000, 2),
+        ("Chief Medical Officer", 75000, 3),
+        ("Science Officer", 70000, 3);
+
+INSERT INTO employees (first_name, last_name, role_id)
+VALUES  ("Kathryn", "Janeway", 1),
+        ("Samantha", "Wildman", 5);
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES  ("Tom", "Paris", 2, 1),
+        ("B'Elanna", "Torres", 3, 1),
+        ("Harry", "Kim", 4, 3),
+        ("The", "Doctor", 5, 1),
+        ("Joe", "Carey", 4, 3);
+
+
+SELECT * FROM departments;
+SELECT * FROM roles;
+SELECT * FROM employees;
