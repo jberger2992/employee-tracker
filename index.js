c70c7936621ffc6c8fd72a8cf7477c3273b08b0b
jberger2992 <jberger2992@gmail.com>
2023-05-01 10:37:57 -0400

    inquirer prompt structure finished

--- index.js
+++ index.js
@@ -24,12 +24,114 @@ const defaultMenu = ()=>{
             message:"What would you like to do?",
             name:"answer",
             choices:["View All Departments", "View All Roles", "View all Employees", "Add a Department", "Add a Role", "Add an Employee", "Update an Employee's Role"]
-        },
+        }
     ]).then(ans=>{
         console.log(ans);
         if(ans.answer === "View All Departments"){
             
+            defaultMenu()
         }
+        if(ans.answer === "View All Roles"){
+            
+            defaultMenu()
+        }
+        if(ans.answer === "View all Employees"){
+            
+            defaultMenu()
+        }
+        if(ans.answer === "Add a Department"){
+            
+        }
+        if(ans.answer === "Add a Role"){
+            
+        }
+        if(ans.answer === "Add an Employee"){
+            
+        }
+        if(ans.answer === "Update an Employee's Role"){
+            
+        }
+    })
+}
+
+const addDepartment = ()=>{
+    inquirer.prompt([
+        {
+            type:"input",
+            message:"What is the name of the department you would like to add?",
+            name:"name"
+        }
+    ]).then(ans=>{
+    
+        defaultMenu()
+    })
+}
+
+const addRole = ()=>{
+    inquirer.prompt([
+        {
+            type:"input",
+            message:"What is the title of the role you would like to add?",
+            name:"title"
+        },
+        {
+            type:"input",
+            message:"What is this role's salary?",
+            name:"salary"
+        },
+        {
+            type:"input",
+            message:"What is the department this role belongs to?",
+            name:"department",
+            choices:[]
+        }
+    ]).then(ans=>{
+    
+        defaultMenu()
+    })
+}
+
+const addEmployee = ()=>{
+    inquirer.prompt([
+        {
+            type:"input",
+            message:"What is the first name of this employee?",
+            name:"fName"
+        },
+        {
+            type:"input",
+            message:"What is the last name of this employee?",
+            name:"lName"
+        },
+        {
+            type:"list",
+            message:"What is this employee's role?",
+            name:"role",
+            choices:[]
+        },
+        {
+            type:"list",
+            message:"Who is this employee's manager?",
+            name:"role",
+            choices:["None"]
+        }
+    ]).then(ans=>{
+    
+        defaultMenu()
+    })
+}
+
+const updateEmployee = ()=>{
+    inquirer.prompt([
+        {
+            type:"list",
+            message:"What role would like to asign to this employee?",
+            name:"role",
+            choices:[]
+        }
+    ]).then(ans=>{
+    
+        defaultMenu()
     })
 }
 
